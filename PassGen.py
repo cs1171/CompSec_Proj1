2390b2d48870e32fbb8d78a51c2161f76c486e6b
cs1171 <cdsoto2@miners.utep.edu>
2018-02-09 00:48:44 -0700

    modified code to search between 6-10 chars, corrected valid chars for password. this version checks against all 5 known hashes

--- PassGen.py
+++ PassGen.py
@@ -3,30 +3,33 @@ import string
 import hashlib
 import time
 
-chars = string.ascii_letters + string.digits + string.punctuation
+chars = string.ascii_letters + string.digits + "-" + "_"
 
 
 def gen():
     start = time.time()
-    for pass_len in range(4, 10):
+    for pass_len in range(6, 10):
         for pwd in itertools.product(chars, repeat=pass_len):
             pwd = ''.join(pwd)
             hashpwd = hashlib.sha1(pwd.encode('utf-8')).hexdigest()
-            if hashpwd == passHash:
-                print(pwd)
+            if hashpwd == (passHash1[1] or passHash2[1] or passHash3[1] or
+                           passHash4[1] or passHash5[1]):
+                for e in passHash:
+                    for j, k in passHash(e):
+                        if hashpwd == passHash(e(k)):
+                            user = passHash(e(j))
                 end = time.time()
-                print(end - start)
-                return pwd
+                print(pwd)
+                with open("cracked.txt", "a", encoding='utf-8') as cracked:
+                    cracked.write("Username: " + user + " Password: " + val + " Time: " + (end-start) + "\n")
 
 
-with open("randPwdFile38.txt", "r", encoding='utf-8') as passFile:
-    for line in passFile:
-        line = line.strip()
-        line = line.split(',')
-        passHash = line[1]
-        uname = line[0]
-        passHash = passHash.replace(' ', '')
-        print(passHash)
-        val = gen()
-        with open("cracked.txt", "a", encoding='utf-8') as cracked:
-            cracked.write("Username: " + uname + " Password: " + val + "\n")
+passHash1 = ('diego38-zpU', '07b8be9ba2686ea94c80836d1d1a4926476a6ed0')
+passHash2 = ('mary38-zpU', 'eeeb6b75308a524fcf050c64204dbba1cc6b45f9')
+passHash3 = ('sofia38-zpU', 'e1f26599aa0d78e5072c4c6ef52ff41dd9cf9ff3')
+passHash4 = ('james38-zpU', '2dec061b79860ffb97efa22a58c632fbe85550ee')
+passHash5 = ('santiago38-zpU', '8794b1a8f46dcbb626cfa4d2510d1998e12cd08d')
+
+passHash = (passHash1, passHash2, passHash3, passHash4, passHash5)
+
+gen()
